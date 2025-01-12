1936242db4c3bed22bcd4d9e6675c379e89afb02
ma-shamshiri <ma.shamshiri@gmail.com>
2025-01-12 17:49:10 -0500

    Update photos of team members

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.4msk4g7k2u8"
+    "revision": "0.cfmkbvkl97"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
